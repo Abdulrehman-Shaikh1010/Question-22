ed6b006a184db8998aaa5c14cd7bdf614053b066
Abdulrehman <you@example.com>
2024-05-23 11:15:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// Array of fruits
+var fruits = ["Apple", "Banana", "Orange"];
+// Accessing the fruit at index 4 (which doesn't exist)
+console.log(fruits[4]);
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Array of fruits
+let fruits: string[] = ["Apple", "Banana", "Orange"];
+
+// Accessing the fruit at index 4 (which doesn't exist)
+console.log(fruits[4]);
